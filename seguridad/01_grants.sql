d54cc6037c58e3f9b4a44eebbf5c575c8fd67822
gcolanq <gustavo19941991@gmail.com>
2026-07-17 23:18:01 -0500

    Add files via upload

--- seguridad/01_grants.sql
+++ seguridad/01_grants.sql
@@ -1,1 +1,17 @@
+-- =============================================
+-- GRANTS DEL PROYECTO
+-- =============================================
 
+USE CATALOG dbw_examen_gustavo;
+
+-- Permisos sobre Bronze
+GRANT USE SCHEMA ON SCHEMA bronze TO `users`;
+GRANT SELECT ON ALL TABLES IN SCHEMA bronze TO `users`;
+
+-- Permisos sobre Silver
+GRANT USE SCHEMA ON SCHEMA silver TO `users`;
+GRANT SELECT ON ALL TABLES IN SCHEMA silver TO `users`;
+
+-- Permisos sobre Gold
+GRANT USE SCHEMA ON SCHEMA gold TO `users`;
+GRANT SELECT ON ALL TABLES IN SCHEMA gold TO `users`;
